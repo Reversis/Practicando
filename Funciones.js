b4f65c1465193ca7bcd705561e9b967677aa7e40
Reversis <Pablodesign0@gmail.com>
2023-07-28 16:27:31 -0600

    Practica JavaScript

---
+++ Funciones.js
@@ -0,0 +1,19 @@
+// 3 maneras de hacer funciones
+
+//PRIMERA
+
+function sumaTres(x) {
+    return x +3;
+}
+
+//SEGUNDA
+
+var sumaTres = function (x) {
+    return x + 3;
+};
+
+//TERCERO
+
+var sumaTres = (x) => {
+    return x + 3;
+};
---
+++ TiposDeDatosBasicos.js
@@ -0,0 +1,18 @@
+// string
+var bootcamp = 'Henry';
+
+//Numeros
+var NumeroEntero = 17;
+var NumeroNegativo = -4;
+var NumeroDecimal = 11.75;
+
+//Booleanos
+var EstoyEntendiendo = true
+var EstoyAburrido = false
+
+//Undefined
+var Cajon1;
+console.log (Cajon1)
+
+//Null
+var sinNada = null;
